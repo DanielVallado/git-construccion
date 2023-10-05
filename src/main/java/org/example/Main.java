0d3b4a62ac3b6ee000e52db1add4afddd260a810
name <morenomariam13@gmail.com>
2023-10-05 16:05:02 -0600

    feat: implementación de resta

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
+    
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public double restar(double numero1, double numero2) {
+        return numero1 - numero2;
+    }
+
 }
