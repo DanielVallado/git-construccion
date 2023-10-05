d3c180f6270daf98c55f37d679b45cfc08bcdbae
esau <esaupiste.ep29@gmail.com>
2023-10-05 15:18:16 -0600

    feat: implementación multiplicación

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    
+    public double multiplicar(double numero1, double numero2) {
+        double resultado = numero1 * numero2;
+        return resultado;
+    }
 }
---
+++ src/main/java/org/example/Multiplicacion.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Multiplicacion {
+
+    public double multiplicar(double numero1, double numero2) {
+        return numero1 * numero2;
+    }
+}    
