7b79807961b488f463789eb841ed0750f409903f
JavierMC <122817675+JAVIERMC33@users.noreply.github.com>
2023-10-05 18:34:52 -0600

    feat: Funcion modulo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public int calcularModulo(int numero, int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException("No se puede dividir por cero.");
+        }
+        return numero % divisor;
+    }
+    
 }
