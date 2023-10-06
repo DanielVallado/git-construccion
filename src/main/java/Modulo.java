f6f40e1fc1c6d986f35255b867f2d0ba200e50bc
Daniel <danielvalladoorozco@gmail.com>
2023-10-05 21:39:26 -0600

    fix: eliminación de archivo

--- src/main/java/Modulo.java
+++
@@ -1,9 +0,0 @@
-public class Modulo{
-
-    public static int calcularModulo(int numero, int divisor) {
-        if (divisor == 0) {
-            throw new ArithmeticException("No se puede dividir por cero.");
-        }
-        return numero % divisor;
-    }
-}
