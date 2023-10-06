b5ad597fa069afa203203e0b91c9982ee6df295a
Daniel <danielvalladoorozco@gmail.com>
2023-10-05 21:44:28 -0600

    fix

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public double sumar(double numero1, double numero2) {
+        return numero1 + numero2;
+    }
+}
