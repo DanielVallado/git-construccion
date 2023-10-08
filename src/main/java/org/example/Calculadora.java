f9e31aca0ccd8892b14ed0ba64113611651c2a2b
JavierMC <122817675+JAVIERMC33@users.noreply.github.com>
2023-10-08 17:57:15 -0600

    Refactor
    Refactorización feature Modulo

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,7 @@
+
+public static int calcularModulo(int numero1, int numero2) {
+        if (numero2 == 0) {
+            throw new ArithmeticException("No se puede dividir por cero.");
+        }
+        return numero1 % numero2;
+    }
