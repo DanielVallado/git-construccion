e0b9e2e9a59b80e393ce586e1cbb278c1159d879
esau <esaupiste.ep29@gmail.com>
2023-10-08 18:00:58 -0600

    refactor: retorno de la función multiplicar y static

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -3,8 +3,7 @@ package org.example;
 public class Calculadora {
 
     public double multiplicar(double numero1, double numero2) {
-        double resultado = numero1 * numero2;
-        return resultado;
+        return numero1 * numero2;
     }
-    
+
 }
