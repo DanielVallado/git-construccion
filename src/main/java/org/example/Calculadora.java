2f93044dc036d5907eb46ab441db985147b9817f
Juan-Trivellari <A21216380@alumnos.uayd.mx>
2023-10-09 01:44:01 -0600

    Modificacion de calculadora

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -2,13 +2,7 @@ package org.example;
 
 
 public class Calculadora {
-    double numerador;
-    double denominador;
-    public Calculadora(double numerador, double denominador){
-        this.numerador = numerador;
-        this.denominador = denominador;
-
-    }
+    
     public static double divison (double numero1, double numero2){
         return (numero1/numero2);
     }
